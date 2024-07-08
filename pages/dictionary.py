5ea3c305cbf70edc5e2dd2cbcafdff2ea3702349
Dayoung-Youn <jessica8349@gmail.com>
2024-07-08 10:24:20 +0900

    Update dictionary.py

--- pages/dictionary.py
+++ pages/dictionary.py
@@ -3,9 +3,9 @@ import pandas as pd
 import plotly.express as px
 
 # 사이드바에 페이지 선택 옵션 설정
-st.sidebar.markdown("[textmining](https://textminingproject.streamlit.app)")
-st.sidebar.markdown("[dictionary](https://textminingdictionary.streamlit.app)")
-st.sidebar.markdown("[MPK tone and BOK baserate](https://mpbtonebokbaserate.streamlit.app)")
+# st.sidebar.markdown("[textmining](https://textminingproject.streamlit.app)")
+# st.sidebar.markdown("[dictionary](https://textminingdictionary.streamlit.app)")
+# st.sidebar.markdown("[MPK tone and BOK baserate](https://mpbtonebokbaserate.streamlit.app)")
 
 st.title("Dictionary Filter")
 
